acc6af4058aab186793d5da6a4f2f33994f55043
NaraCamargo <naraliggia2@gmail.com>
2023-09-20 08:54:21 -0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Sapato from './pages/Sapato/index';
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Modelos/>
+  </React.StrictMode>
+);
+
