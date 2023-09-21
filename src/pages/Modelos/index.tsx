66c77860539b536fdd76bac33be042a48815b382
NaraCamargo <naraliggia2@gmail.com>
2023-09-21 08:59:57 -0300

    21/09

--- src/pages/Modelos/index.tsx
+++ src/pages/Modelos/index.tsx
@@ -1,5 +1,28 @@
+import styles from './Modelos.module.scss';
+import Logo from 'assets/logo.svg';
+import Buscador from './Buscador';
+import { useState } from 'react';
+import Filtros from './Filtros';
+
 export default function Modelos(){
+    const [busca, setBusca] = useState(""); 
     return(
-        <div>Modelos</div>
+        <main>
+        <nav className={styles.lista}>
+            <img src={Logo} alt="Logo dos Sapatos" />
+        </nav>
+        <header className={styles.header}>
+            <div className={styles.header__text}>
+             Sapatos em liquidação!
+            </div>
+        </header>      
+        <section className={styles.modelos}>
+            <h3 className={styles.modelos__titulo}>Modelos</h3>
+            <Buscador busca={busca} setBusca={setBusca}/> 
+            <div className={styles.modelos__filtros}>
+            <Filtros/>
+            </div>
+        </section>
+    </main>
     );
 }
